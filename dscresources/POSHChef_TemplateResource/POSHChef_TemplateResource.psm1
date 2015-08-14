2f7b1d04ad0defdc15db1d2c6d9cac5147cc9240
Omar Rizwani <omarr@asos.com>
2015-08-14 11:29:19 +0000

    Temporary fix to POSHChef_Template Resource to get Boxes Configuring with errors...
    git-tfs-id: [http://tfs:8080/tfs/ASOS]$/ASOS/BuildAndDeploy/OpenSourceProjects/POSHChef/Release/src/POSHChef;C172143

--- dscresources/POSHChef_TemplateResource/POSHChef_TemplateResource.psm1
+++ dscresources/POSHChef_TemplateResource/POSHChef_TemplateResource.psm1
@@ -290,3 +290,92 @@ function Test-TargetResource
 
 	return $test
 }
+
+function Expand-Template {
+
+    [CmdletBinding()]
+    param (
+
+        [String]
+        [Parameter(ParameterSetName="string")]
+        [alias("template")]
+        # Template that is to be expanded
+        $InputObject,
+
+        [string]
+        [Parameter(ParameterSetname="file")]
+        # Path to a file that contains the string to be patched
+        $path,
+
+        [string]
+        # Beginning tag denoting the start of an expression
+        $beginTag = "[[",
+
+        [string]
+        # Ending tag denoting the end of an expression
+        $endTag = "]]",
+
+        [alias("attributes")]
+        $node,
+
+        $variables
+
+    )
+
+    # if the parameterset called is 'file' read the contents into the InputObject
+    if ($PSCmdlet.ParameterSetname -eq "file") {
+
+      # Check that the file exists
+      if (!(Test-Path -Path $path)) {
+        Write-Log -Message ("Template file cannot be located: {0}" -f $path) -LogLevel Verbose
+      } else {
+        $inputObject = Get-Content -Path $path -Raw
+      }
+    }
+
+    # Append an ending expression to the inputobject so that the whole string is evaluated
+    $inputObject += "`n{0} {1}" -f $beginTag, $endTag
+
+    # Ensure the tags are escaped
+	  $optionTagRaw = $beginTag[0]
+	  $optionTag = [Regex]::Escape($optionTagRaw)
+    $beginTag = [Regex]::Escape($beginTag)
+    $endTag = [Regex]::Escape($endTag)
+
+    # Set the output string
+    $output = [String]::Empty
+
+    # Build up the expression to match with
+	  $pattern = "(?sm)(?<pre>.*?)[{0}?](?!{1})(?<exp>.*?){2}(?<post>.*?)$" -f $optionTag, $beginTag, $endTag
+
+    # Create a Regex object using the pattern from above
+    # This uses a .Net classes so that the named capture groups work.  The built in -match function did not cut it
+    $regex = [regex] $pattern
+
+    # Create a string builder in which the rendered template will be created
+    $rendered = New-Object System.Text.StringBuilder
+
+    foreach ($match in $regex.Matches($InputObject)) {
+
+        # Add each component to the rendered template
+        $rendered.Append(($match.Groups["pre"].Value)) | Out-Null
+
+        # evaluate the expression if one exists
+		    $expression = $match.Groups["exp"].Value.Trim($optionTagRaw)
+        if (![String]::IsNullOrEmpty($expression)) {
+
+            # invoke the expression
+            $evaluated = (Invoke-Expression $expression | Out-String).Trim()
+            $rendered.Append($evaluated) | Out-Null
+        }
+
+        $rendered.Append(($match.Groups["post"].Value)) | Out-Null
+
+    }
+
+    # Return the rendered template
+    $rendered = $rendered.ToString().SubString(0, $rendered.ToString().Length - 1)
+    return $rendered
+
+}
+
